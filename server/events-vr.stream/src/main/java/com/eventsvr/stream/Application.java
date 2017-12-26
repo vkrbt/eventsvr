481fe18849f912148e9d85763c0ad38c70a5c5d0
Misha Novik <novik.misha.1@gmail.com>
2017-12-26 23:58:32 +0300

    add security to stream microservice

--- server/events-vr.stream/src/main/java/com/eventsvr/stream/Application.java
+++ server/events-vr.stream/src/main/java/com/eventsvr/stream/Application.java
@@ -1,12 +1,37 @@
 package com.eventsvr.stream;
 
 
+import com.eventsvr.domain.service.UserService;
+import com.eventsvr.domain.service.impl.UserDetailsServiceImpl;
+import com.eventsvr.domain.service.impl.UserServiceImp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.regex.Pattern;
 
 @SpringBootApplication
+@EnableCassandraRepositories("com.eventsvr.domain.repository")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetails() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public UserService userService() {
+        return new UserServiceImp();
     }
 }
